eb54efcd8439729c51200cbbc1ae87012a604ffb
mk-128 <mk128@example.com>
2026-07-17 13:32:47 +0900

    auto update 2026-07-17 13:32

--- scraper.py
+++ scraper.py
@@ -46,6 +46,8 @@ TOUR_URLS = {
     "kayabuki_himawari": "https://travel.mk-group.co.jp/tourkyoto/kayabuki-himawari/",
     "eigamura-jidaigeki": "https://travel.mk-group.co.jp/tourkyoto/eigamura-jidaigeki/",
 
+    "chikubushima": "https://travel.mk-group.co.jp/tourkyoto/chikubushima2026/",
+
 }
 
 HEADERS = {
